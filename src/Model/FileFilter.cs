dbf61f97f118c6a9b6485c6b4215ab07aeb87205
Vitor Paulino <vitorpaulinog@gmail.com>
2024-05-16 23:16:09 -0300

    Add FileFilter class to filter files by a pattern
    - Use Directory.GetFiles
    - Don't use Regex
    - Use one overload to get more than one directories and patterns

---
+++ src/Model/FileFilter.cs
@@ -0,0 +1,27 @@
+using System.Text;
+using System.Text.RegularExpressions;
+namespace FileDominator.Model;
+public static class FileFilter
+{
+    public static List<string> FilterFiles(string directory, string pattern)
+    {
+        List<string> filePaths = new(); 
+        var fileNames = Directory.GetFiles(directory, pattern).ToList();
+        return fileNames;
+    }
+
+    public static List<string> FilterFiles(List<string> directories, List<string> patterns)
+    {
+        List<string> filePaths = new();
+
+        foreach (var directory in directories)
+        {
+            foreach (var pattern in patterns)
+            {
+                filePaths.AddRange(FilterFiles(directory, pattern));
+            }
+        }
+
+        return filePaths;
+    }
+}
